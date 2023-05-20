f3225d0e467d30201b77e812a3d7f5ae17fb37d7
Washington Irving <iamwashingyou@gmail.com>
2023-05-20 03:40:02 +0300

    add user config

--- main.go
+++ main.go
@@ -3,12 +3,19 @@ package main
 import (
 	argparser "github.com/IrvingWash/klezmer/arg_parser"
 	musicreader "github.com/IrvingWash/klezmer/music_reader"
+	userconfig "github.com/IrvingWash/klezmer/user_config"
 )
 
 func main() {
 	args := argparser.New()
 
-	musicReader := musicreader.New(args.GetMusicPath())
+	userConfig := userconfig.New()
+
+	if args.GetMusicPath() != "" {
+		userConfig.SetMusicPath(args.GetMusicPath())
+	}
+
+	musicReader := musicreader.New(userConfig.GetMusicPath())
 
 	if args.GetDecide() {
 		musicReader.GetRandom()
--- user_config/user_config.go
+++ user_config/user_config.go
@@ -1,5 +1,69 @@
 package userconfig
 
+import (
+	"bufio"
+	"encoding/json"
+	"io/fs"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+)
+
 type UserConfig struct {
-	musicPath string
+	MusicPath string `json:"musicPath"`
+}
+
+func New() *UserConfig {
+	userConfig := UserConfig{}
+
+	return parseConfig(&userConfig)
+}
+
+func (uc *UserConfig) SetMusicPath(path string) {
+	uc.MusicPath = path
+
+	jsonBytes, err := json.Marshal(uc)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = ioutil.WriteFile(filepath.Join("user_config", "user-config.json"), jsonBytes, fs.ModeAppend)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func (uc *UserConfig) GetMusicPath() string {
+	return uc.MusicPath
+}
+
+func parseConfig(userConfig *UserConfig) *UserConfig {
+	file, err := os.Open(filepath.Join("user_config", "user-config.json"))
+
+	defer file.Close()
+
+	if err != nil {
+		return userConfig
+	}
+
+	fileStat, err := file.Stat()
+
+	if err != nil {
+		return userConfig
+	}
+
+	jsonBytes := make([]byte, fileStat.Size())
+
+	_, err = bufio.NewReader(file).Read(jsonBytes)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	json.Unmarshal(jsonBytes, &userConfig)
+
+	return userConfig
 }
